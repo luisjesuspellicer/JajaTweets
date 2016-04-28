a15758955d9ef06e00cfd19e45eb8543fdc78191
diego <dcerearr@gmail.com>
2016-04-28 17:21:58 +0200

    expiry date changed

--- users/models/users.model.js
+++ users/models/users.model.js
@@ -48,7 +48,7 @@
     // Creates the Jwt from user's info and returns it.
     userSchema.methods.generateJwt = function() {
         var expiry = new Date();
-        expiry.setDate(expiry.getDate() + 7);
+        expiry.setDate(expiry.getDate() + 1);
 
         return jwt.sign({
             _id: this._id,
